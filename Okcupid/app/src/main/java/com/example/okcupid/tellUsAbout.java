be18e79101238723c40ba0c9e63646338b7b79a6
MINTU325 <sainimintu34@gmail.com>
2020-12-31 20:11:53 +0530

    mintu 12

--- Okcupid/app/src/main/java/com/example/okcupid/tellUsAbout.java
+++ Okcupid/app/src/main/java/com/example/okcupid/tellUsAbout.java
@@ -2,13 +2,28 @@ package com.example.okcupid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class tellUsAbout extends AppCompatActivity {
+    private Button Btnnext208;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tell_us_about);
+        Btnnext208 = findViewById(R.id.btnnext208);
+        Btnnext208.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(tellUsAbout.this, addPhotoOfYou.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }

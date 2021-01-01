1f109d5a557565dc31da93cf8325d158fcab1726
MINTU325 <sainimintu34@gmail.com>
2021-01-01 21:45:47 +0530

    mintu

--- Okcupid/app/src/main/java/com/example/okcupid/email.java
+++ Okcupid/app/src/main/java/com/example/okcupid/email.java
@@ -6,29 +6,68 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class email extends AppCompatActivity {
     private Button mBtnnext;
+    private EditText mEditemailid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
-        mBtnnext = findViewById(R.id.btnnext);
+        mBtnnext = findViewById(R.id.btnnextemail);
+        mEditemailid = findViewById(R.id.etemailid);
         mBtnnext.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(email.this,password.class);
-                startActivity(intent);
+                if (isCredValid()){
+
+                    Intent intent = new Intent(email.this,password.class);
+                    startActivity(intent);
+
+
+                }
+
+
             }
         });
     }
 
-
-
     public  void previous(View view){
         startActivity(new Intent(email.this, MainActivity.class));
     }
+
+    private  boolean isCredValid(){
+        boolean isDataValid  = true;
+        if(!mEditemailid.getText().toString().contains("@gmail.com")){
+            mEditemailid.setError("Invalid email id");
+            isDataValid = false;
+        }
+
+        return isDataValid;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- Okcupid/app/src/main/java/com/example/okcupid/password.java
+++ Okcupid/app/src/main/java/com/example/okcupid/password.java
@@ -6,25 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class password extends AppCompatActivity {
-    private  Button  mbtbSingUp;
+    private Button mbtbSingUp;
+    private EditText EditPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
-       mbtbSingUp= findViewById(R.id.btnsignup);
+        mbtbSingUp = findViewById(R.id.btnsignup);
+        EditPassword= findViewById(R.id.etemailid);
+
+
         mbtbSingUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(password.this,Set_up.class);
-                startActivity(intent);
+                    Intent intent = new Intent(password.this, Set_up.class);
+                    startActivity(intent);
+
+
+
+
             }
         });
     }
-    public  void previous(View view){
+
+    public void previous(View view) {
         startActivity(new Intent(password.this, email.class));
     }
 
-
-}
+    }
